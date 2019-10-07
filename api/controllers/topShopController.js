7b97cf04c3c2b71257872b5222bbb0d2758b8d96
Robson Gonçalves <robson.goncalves@adp.com>
2019-10-07 11:10:10 -0300

    removed commented code

--- api/controllers/topShopController.js
+++ api/controllers/topShopController.js
@@ -1,56 +1,33 @@
 'use strict';
 
-
-// var mongoose = require('mongoose'),
-//   Shop = mongoose.model('Shops');
-
 exports.list_all_shops = function(req, res) {
-  const yelp = require('yelp-fusion');
+  Shop.find({}, function(err, shop) {
 
-  const apiKey = '8ZrKLjDVqCgbAL4uh736OZev15c-MIMuJ8kCDm1NfUDJ8vAItJwY2cWTamsslYXuFZG_SWm_O2GB8h-9asfrLQUtyBafmHe7fZmz0GP29dGa6sCK1B_tSfzy7qCAXXYx';
+      const yelp = require('yelp-fusion');
 
-  const searchRequest = {
-    term: 'restaurants',
-    location: 'Los Angeles',
-  };
+      const apiKey = '8ZrKLjDVqCgbAL4uh736OZev15c-MIMuJ8kCDm1NfUDJ8vAItJwY2cWTamsslYXuFZG_SWm_O2GB8h-9asfrLQUtyBafmHe7fZmz0GP29dGa6sCK1B_tSfzy7qCAXXYx';
 
-  const client = yelp.client(apiKey);
+      const searchRequest = {
+      term: 'ice cream shop',
+      location: 'Alpharetta',
+      };
 
-  client.search(searchRequest)
-    .then((response) => {
-      console.log(response.jsonBody);
-      return response
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-}
-// output should include
-//     business name
-//     business address (street, city)
-//     excerpt from a review of that business
-//     name of the person that wrote the review
-//     business information should be output in the order received from the API response
-
-// exports.list_all_shops = function(req, res) {
-//   Shop.find({}, function(err, shop) {
-//     if (err)
-//       res.send(err);
-//     res.json(shop);
-//   });
-// };
+      const client = yelp.client(apiKey);
 
 
+      client.search(searchRequest)
+        .then((response) => {
+            console.log(response.jsonBody);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
 
-// exports.create_a_shop = function(req, res) {
-//   var new_shop = new Shop(req.body);
-//   Shop.save(function(err, shop) {
-//     if (err)
-//       res.send(err);
-//     res.json(shop);
-//   });
-// };
-
+      if (err)
+        res.send(err);
+      res.json(shop);
+});
+};
 
 exports.read_a_shop = function(req, res) {
     Shop.findById(req.params.shopId, function(err, shop) {
@@ -60,24 +37,3 @@ exports.read_a_shop = function(req, res) {
   });
 };
 
-
-// exports.update_a_shop = function(req, res) {
-//   Shop.findOneAndUpdate({_id: req.params.shopId}, req.body, {new: true}, function(err, shop) {
-//     if (err)
-//       res.send(err);
-//     res.json(shop);
-//   });
-// };
-
-
-// exports.delete_a_shop = function(req, res) {
-
-
-//   Shop.remove({
-//     _id: req.params.shopId
-//   }, function(err, shop) {
-//     if (err)
-//       res.send(err);
-//     res.json({ message: 'Shop successfully deleted' });
-//   });
-// };
--- server.js
+++ server.js
@@ -2,12 +2,6 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000;
 
-// Task = require('./api/models/todoListModel'), //created model loading here
-// bodyParser = require('body-parser');
-
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-
 // const config = require('./config.js')
 
 var routes = require('./api/routes/topShopsRoutes');

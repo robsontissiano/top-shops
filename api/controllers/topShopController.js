e3f0bd8a7a14a60c824e0a233ccfbfd18e24eaa2
Robson Gonçalves <robson.goncalves@adp.com>
2019-10-08 09:50:29 -0300

    updated getShops method'

--- api/controllers/topShopController.js
+++ api/controllers/topShopController.js
@@ -1,32 +1,37 @@
 'use strict';
 
 exports.list_all_shops = function(req, res) {
-  Shop.find({}, function(err, shop) {
+  // Shop.find({}, function(err, shop) {
 
-      const yelp = require('yelp-fusion');
+    const yelp = require('yelp-fusion');
 
-      const apiKey = '8ZrKLjDVqCgbAL4uh736OZev15c-MIMuJ8kCDm1NfUDJ8vAItJwY2cWTamsslYXuFZG_SWm_O2GB8h-9asfrLQUtyBafmHe7fZmz0GP29dGa6sCK1B_tSfzy7qCAXXYx';
+    const apiKey = '8ZrKLjDVqCgbAL4uh736OZev15c-MIMuJ8kCDm1NfUDJ8vAItJwY2cWTamsslYXuFZG_SWm_O2GB8h-9asfrLQUtyBafmHe7fZmz0GP29dGa6sCK1B_tSfzy7qCAXXYx';
 
-      const searchRequest = {
+    const searchRequest = {
       term: 'ice cream shop',
       location: 'Alpharetta',
-      };
+    };
 
-      const client = yelp.client(apiKey);
+    const client = yelp.client(apiKey);
 
 
-      client.search(searchRequest)
-        .then((response) => {
-            console.log(response.jsonBody);
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+    client.search(searchRequest)
+      // .then((response) => {
+      //     console.log(response.jsonBody);
+      //     return response
+      // })
+      .then(function(result){
+        console.log(result)
+        res.json(result);
+      })
+      .catch((error) => {
+          console.log(error);
+      });
 
-      if (err)
-        res.send(err);
-      res.json(shop);
-});
+    // if (err)
+    //   res.send(err);
+    // res.json(shop);
+  // });
 };
 
 exports.read_a_shop = function(req, res) {
--- api/routes/topShopsRoutes.js
+++ api/routes/topShopsRoutes.js
@@ -3,12 +3,12 @@ module.exports = function(app) {
   var topShop = require('../controllers/topShopController');
 
   // topShop Routes
-  app.route('/shop')
-    .get(topShop.list_all_shop);
+  app.route('/shops')
+    .get(topShop.list_all_shops);
     // .post(topShop.create_a_shop);
 
 
-  app.route('/shop/:shopId')
+  app.route('/shops/:shopId')
     .get(topShop.read_a_shop);
     // .put(topShop.update_a_shop)
     // .delete(topShop.delete_a_shop);
